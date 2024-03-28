7c4992b6d4db32d2f78b4c760592b3adf2184221
RafsanaKabirSamanta <rafsana@ibos.com>
2024-03-28 10:49:59 +0600

    scroll up and down

--- src/test/java/Setup.java
+++ src/test/java/Setup.java
@@ -2,18 +2,25 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 import java.time.Duration;
 
 public class Setup {
+
+
+
     public WebDriver driver;
     @BeforeTest
     public void setup(){
 
-        WebDriverManager.chromedriver().setup();
-        driver=new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver","C:\\Automation Project\\chromedriver-win64\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("C:\\Users\\hp\\Downloads\\chrome-win64 (1)\\chrome-win64\\chrome.exe");
+        //WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.openqa.selenium.JavascriptExecutor;
 
 import java.util.List;
 
@@ -25,71 +26,94 @@ public class TestRunner extends Setup{
 
     @Test(priority = 2 , testName = "Select item")
     public void selectItem() throws InterruptedException{
+        Thread.sleep(3000);
 
-        Thread.sleep(2000);
-        List<WebElement> selectItem1 = driver.findElements(By.className("inventory_item_name"));
-        selectItem1.get(0).click();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        Thread.sleep(1000);
+        js.executeScript("window.scrollBy(0,1000)", "");
+        Thread.sleep(1000);
+        js.executeScript("window.scrollBy(0,-1000)", "");
+
+
+
+//        Thread.sleep(2000);
+//        List<WebElement> selectItem1 = driver.findElements(By.className("inventory_item_name"));
+//        selectItem1.get(0).click();
 
     }
     //add item to cart
-    @Test(priority = 3 , testName = "Item add to cart button")
-    public void addToCart() throws InterruptedException{
-
-        Thread.sleep(2000);
-        List<WebElement> addCart = driver.findElements(By.id("add-to-cart-sauce-labs-backpack"));
-
-        addCart.get(0).click();
-
-    }
-
-    // go to cart
-    @Test(priority = 4 , testName = "Select Cart icon")
-    public void cartIcon() throws InterruptedException{
-
-        Thread.sleep(2000);
-        List<WebElement> addCart = driver.findElements(By.className("shopping_cart_link"));
-
-        addCart.get(0).click();
-
-    }
-
-    @Test(priority = 5 , testName = "CheckOut")
-    public void checkOut() throws InterruptedException{
-
-        Thread.sleep(2000);
-        List<WebElement> checkout = driver.findElements(By.id("checkout"));
-
-        checkout.get(0).click();
-
-    }
-    @Test(priority = 6 , testName = "Form fillup")
-    public void formFill() throws InterruptedException{
-
-        Thread.sleep(2000);
-        List<WebElement> form = driver.findElements(By.className("form_input"));
-
-        form.get(0).sendKeys("Rafsana"); //First Name
-        form.get(1).sendKeys("Kabir"); //Last Name
-        form.get(2).sendKeys("1216"); //Zipcode
-
-        List<WebElement> continueBtn = driver.findElements(By.id("continue"));
-        continueBtn.get(0).click();
-    }
-
-    @Test(priority = 7 , testName = "Back to landing")
-    public void finish() throws InterruptedException{
-
-        Thread.sleep(2000);
-        //finish shopping
-        List<WebElement> Finish = driver.findElements(By.id("finish"));
-
-        Finish.get(0).click();
-
-        Thread.sleep(2000);
-        // back to menu
-        List<WebElement> back = driver.findElements(By.id("back-to-products"));
-        back.get(0).click();
-    }
+//    @Test(priority = 3 , testName = "Item add to cart button")
+//    public void addToCart() throws InterruptedException{
+//
+//        Thread.sleep(2000);
+//        List<WebElement> addCart = driver.findElements(By.id("add-to-cart"));
+//
+//        addCart.get(0).click();
+//
+//    }
+//
+//    @Test(priority = 4 , testName = "Go back to home and select another item")
+//    public void addAnotherItemToCart() throws InterruptedException{
+//        Thread.sleep(2000);
+//        //back to products
+//        List<WebElement> backtoLanding = driver.findElements(By.id("back-to-products"));
+//        backtoLanding.get(0).click();
+//
+//        // select another item and add to cart
+//        Thread.sleep(1000);
+//        List<WebElement> anotherItem = driver.findElements(By.id("add-to-cart-sauce-labs-bike-light"));
+//        anotherItem.get(0).click();
+//
+//    }
+//
+//    // go to cart
+//    @Test(priority = 5 , testName = "Select Cart icon")
+//    public void cartIcon() throws InterruptedException{
+//
+//        Thread.sleep(2000);
+//        List<WebElement> addCart = driver.findElements(By.className("shopping_cart_link"));
+//
+//        addCart.get(0).click();
+//
+//    }
+//
+//    @Test(priority = 6 , testName = "CheckOut")
+//    public void checkOut() throws InterruptedException{
+//
+//        Thread.sleep(2000);
+//        List<WebElement> checkout = driver.findElements(By.id("checkout"));
+//
+//        checkout.get(0).click();
+//
+//    }
+//    @Test(priority = 7 , testName = "Form fillup")
+//    public void formFill() throws InterruptedException{
+//
+//        Thread.sleep(2000);
+//        List<WebElement> form = driver.findElements(By.className("form_input"));
+//
+//        form.get(0).sendKeys("Rafsana"); //First Name
+//        form.get(1).sendKeys("Kabir"); //Last Name
+//        form.get(2).sendKeys("1216"); //Zipcode
+//
+//        List<WebElement> continueBtn = driver.findElements(By.id("continue"));
+//        continueBtn.get(0).click();
+//    }
+//
+//    @Test(priority = 8 , testName = "Back to landing")
+//    public void finish() throws InterruptedException{
+//
+//        Thread.sleep(2000);
+//        //finish shopping
+//        List<WebElement> Finish = driver.findElements(By.id("finish"));
+//
+//        Finish.get(0).click();
+//
+//        Thread.sleep(2000);
+//        // back to menu
+//        List<WebElement> back = driver.findElements(By.id("back-to-products"));
+//        back.get(0).click();
+//    }
 
 
 
